b1785509f7e538e0c8c0138f0f38d4967c13d2d7
sheikh-arafat <sheikh.mohammad.arafat@g.bracu.ac.bd>
2022-06-22 18:23:04 +0600

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Test-Login-Automation
+AUTOMATION TESTING USING SELENIUM WEBDRIVER
+
+The automation script for log in, page navigating and web scrapping was written using python. Please, watch the video "loginAutomation.mp4" for the proper guidance.
---
+++ login-automation.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+
+Username='sheikhmohammadarafat@gmail.com'
+Password="123@@abc"
+
+driver=webdriver.Chrome('chromedriver.exe')
+driver.get('https://www.imdb.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.imdb.com%2Fregistration%2Fap-signin-handler%2Fimdb_us&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=imdb_us&openid.mode=checkid_setup&siteState=eyJvcGVuaWQuYXNzb2NfaGFuZGxlIjoiaW1kYl91cyIsInJlZGlyZWN0VG8iOiJodHRwczovL3d3dy5pbWRiLmNvbS8_cmVmXz1sb2dpbiJ9&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&tag=imdbtag_reg-20')
+
+user_input=driver.find_element_by_id("ap_email")
+user_input.send_keys(Username)
+user_pass=driver.find_element_by_id("ap_password")
+user_pass.send_keys(Password)
+
+login = driver.find_element_by_id("signInSubmit")
+login.click()
